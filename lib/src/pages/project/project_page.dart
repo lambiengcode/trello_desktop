699b9d9efb27a6dfe3cb073cab1d735d25ff666c
lambiengcode <lambiengcode@gmail.com>
2021-04-17 10:24:50 +0700

    [ui] done column

--- lib/src/pages/project/project_page.dart
+++ lib/src/pages/project/project_page.dart
@@ -47,7 +47,71 @@ class _ProjectPageState extends State<ProjectPage> {
               ),
             ],
           ),
+          SizedBox(height: _size.height / 30.0),
+          Row(
+            children: [
+              _buildColumnTask(context, 'Todo'),
+              SizedBox(width: 24.0),
+              _buildColumnTask(context, 'Progress'),
+              SizedBox(width: 24.0),
+              _buildColumnTask(context, 'Done'),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildColumnTask(context, title) {
+    final _size = MediaQuery.of(context).size;
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 24.0,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          color: colorPrimary.withOpacity(.2),
+        ),
+        child: Column(
+          children: [
+            // SizedBox(height: 12.0),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: colorDarkGrey,
+                      fontSize: _size.height / 54.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 12.0,
+                      vertical: 8.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: colorPrimary.withOpacity(.5),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Text(
+                      '2',
+                      style: TextStyle(
+                        color: colorTitle,
+                        fontSize: _size.height / 58.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
